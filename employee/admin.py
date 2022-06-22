088a4ed1f3a84add82289cb8a9296c377eb15c50
unknown <rohandoshi008@gmail.com>
2022-06-22 17:27:58 +0530

    Commited

---
+++ employee/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(EmployeeAddressInfo)
+admin.site.register(EducationInfo)
+admin.site.register(TrainingInfo)
+admin.site.register(EmployeeJobInfo)
+admin.site.register(ExperienceInfo)
+admin.site.register(PersonalInfo)
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\91993\desktop\online course certification\online course certification\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
